8db7c94c0367b25328826fe61eb344df1dc127b0
David Barbero <db00300@my.westga.edu>
2024-02-21 08:51:07 -0500

    remove unneede properties in PanelState class

--- Models/PanelState.cs
+++ Models/PanelState.cs
@@ -1,15 +1,9 @@
 ﻿using System.Collections.Generic;
-using System.Drawing;
-using System.Windows.Forms;
 
 namespace ScratchRentMe.Models
 {
     public class PanelState
     {
-        public Color BackColor { get; set; }
-        public Color ForeColor { get; set; }
-        public BorderStyle BorderStyle { get; set; }
-        public bool Enabled { get; set; }
         public Dictionary<string, object> Args { get; set; }
     }
 }
