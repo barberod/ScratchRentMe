14368a5ce1167454410f1b9c594bd18ada719992
David Barbero <db00300@my.westga.edu>
2024-02-22 08:35:37 -0500

    change variable name of target

--- UserControls/DummyUserControl.cs
+++ UserControls/DummyUserControl.cs
@@ -9,20 +9,20 @@ namespace ScratchRentMe.UserControls
     public partial class DummyUserControl : UserControl
     {
         public new Dictionary<string, object> Tag { get; set; }
-        public Dummy selectedDummy { get; set; }
+        public Dummy GivenDummy { get; set; }
 
         public DummyUserControl(object tag)
         {
             InitializeComponent();
             Tag = (Dictionary<string, object>)tag;
-            selectedDummy = (Dummy)Tag["dummy"];
+            GivenDummy = (Dummy)Tag["dummy"];
             Load += DummyUserControl_Load;
         }
 
         private void DummyUserControl_Load(object sender, EventArgs e)
         {
-            label1.Text = selectedDummy.Name;
-            label2.Text = selectedDummy.Id.ToString();
+            label1.Text = GivenDummy.Name;
+            label2.Text = GivenDummy.Id.ToString();
         }
     }
 }
